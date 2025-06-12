79e66199eecc469222b08b982163dbed0b9c1ef5
Nasim Kourrini <nasimkourrini@Nasims-MacBook-Air.local>
2025-06-12 23:47:52 +0100

    feat: add Supabase client configuration

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -4,4 +4,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://fkfsuglsbexqyllmufak.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrZnN1Z2xzYmV4cXlsbG11ZmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NzQ1MzQsImV4cCI6MjA2MzM1MDUzNH0.zpgg6TlJaTp7bs0xXDdsyvsAconNWIlibphUmCzZM9I'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+    flowType: 'pkce' // Recommended for Next.js
+  }
+});
